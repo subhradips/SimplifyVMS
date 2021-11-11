4e96a2fede41a42f3f6de208b7af9f161b12ed82
Subhradips <subhradip@sb3inc.com>
2021-11-12 02:33:53 +0530

    2nd time

--- src/main/java/demo/SimplifyVMSBase.java
+++ src/main/java/demo/SimplifyVMSBase.java
@@ -1,8 +1,11 @@
 package demo;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 public class SimplifyVMSBase {
 
@@ -12,13 +15,18 @@ public class SimplifyVMSBase {
     static {
         //System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/driver/geckodriver.exe");
 //        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver.exe");
-      System.setProperty("webdriver.chrome.driver","driver/chromedriver2.exe");
-        driver = new ChromeDriver();
+//      System.setProperty("webdriver.chrome.driver","driver/chromedriver2.exe");
+//        driver = new ChromeDriver();
 
 //        ChromeOptions chromeOptions = new ChromeOptions();
 //        chromeOptions.setBinary("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe");
 //
 //        driver = new ChromeDriver(chromeOptions);
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 }
